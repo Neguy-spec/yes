8dfde53ff28871c14ec82211062d658b52f8338b
Neguy-spec <shesh7365@gmail.com>
2026-05-30 03:22:36 +0000

    rewrote things

--- FILES/rahhh.swift
+++ FILES/rahhh.swift
@@ -1,3 +1,17 @@
-print ("wsp dawg")
-var letung: Int = 9
-print (letung)
+import SwiftUI
+
+@main
+struct RahhhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SideStore!")
+            .padding()
+    }
+}
